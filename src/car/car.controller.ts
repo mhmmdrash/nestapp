4f3a34344de26b0ac0782cf98e9edf5893b622b6
Muhammed rasheed <rasheed@rapidinnovation.dev>
2022-11-30 16:42:27 +0530

    some changes

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { CarService } from './car.service'
 
 @Controller('car')
-export class CarController {}
+export class CarController {
+    constructor(private carService: CarService) {}
+
+    @Get()
+    async getCars() {
+        const cars = await this.carService.getCars();
+        return cars;
+    }
+
+    @Get(':carId')
+    async getCar(@Param('carId') carId) {
+        const car = await this.carService.getCar(carId);
+        return car;
+    }
+
+    @Post()
+    async addCar(@Body() createCarDto: CreateCarDto) {
+        const car = await this.carService.addCar(createCarDto);
+        return car;
+    }
+
+    @Delete()
+    async deleteCar(@Query() query) {
+        const cars = await this.carService.deleteCar(query.carId);
+        return cars;
+    }
+
+}
